f1e550119a5790d07e2c2fd7a37caa0622a6b3a6
devin177 <36557400+devin177@users.noreply.github.com>
2018-02-18 09:25:12 -0800

    Update New Javascript.js

--- New Javascript.js
+++ New Javascript.js
@@ -7,14 +7,24 @@ let lagtime = 0;
 var spaceship_image = new Image;
 spaceship_image.src = "spaceshipSprite.png";
 
-let spaceship = new Object();
-spaceship.x = canvas.width/2 - 87/4;
-spaceship.y = canvas.height - 72.5;
-spaceship.velocity = 3
 
 
 let spaceBackground = new Image;
 spaceBackground.src = "spaceBackground.jpg";
+
+
+let bulletImage = new Image;
+bulletImage.src = "bulletSprite.png";
+
+let enemyImage = new Image;
+enemyImage.src = "enemy.png";
+
+
+
+let spaceship = new Object();
+spaceship.x = canvas.width/2 - 87/4;
+spaceship.y = canvas.height - 72.5;
+spaceship.velocity = 5;
 
 // variables that represent buttons presses
 let LEFT = 37;
@@ -24,10 +34,7 @@ let DOWN = 40;
 let SPACE = 32;
 let controller = {};
 
-let bullet = new Object();
-let bulletCounter = 0;
-bullet.x = [];
-bullet.y = [];
+
 
 
 //function that is called when a button is pressed/let go of that checks the keycode of the buttons
@@ -65,21 +72,11 @@ function parse_buttons(){
 		spaceship.x += spaceship.velocity;
 	}
 }
-
-
-		//make 2 arrays called x and y for bullets and in firegun push x and y coordinates into it the x and y would be relative to the ship
-		//make a function that draws the bullets at those coordinates
-		//make a function that adds/subtracts a number from the y coordinate y[i] - 6;
+let bulletTimer = 0;
 function parse_gun(){
 	if(controller.space){
-		bullet.x.push(spaceship.x);
-		bullet.y.push(spaceship.y);
-		firegun();
+		create_bullet();
 	}
-}
-function firegun(){
-	draw_bullet();
-	setTimeout("firegun();", 1000/144);
 }
 
 
@@ -91,44 +88,82 @@ document.onkeyup = function(e){
 	key_state(e.keyCode, false);
 }
 
-
+function create_bullet(){
+	bullets.x.push(spaceship.x);
+	bullets.y.push(spaceship.y);
+}
 function draw_background(){
-
 	ctx.drawImage(spaceBackground, 0,0, canvas.width,canvas.height);
-	}
+}
 
 function draw_characters(){
 	ctx.drawImage(spaceship_image, spaceship.x ,spaceship.y, 87/3, 146/3);
 }
 
-function draw_bullet(){
-	let bulletImage = new Image;
-	bulletImage.src = "bulletSprite.png";
-	ctx.drawImage(bulletImage, bullet.x[bulletCounter]+25/3, bullet.y[bulletCounter]-357/8, 25/2, 357/8);
-	bullet.y[bulletCounter] -= 1;
+function draw_bullet(i){
+	ctx.drawImage(bulletImage, bullets.x[i]+25/3, bullets.y[i]- 357/8, 25/2, 357/8);
+}
+function update(){
+	for(var i = 0; i < bullets.x.length; i ++){
+		if(bullets.y[i] <= -318/8){		
+			bullets.x.splice(i,1);
+			bullets.y.splice(i,1);
+		}
+	}
+	for(var i = 0; i < bullets.x.length; i ++){
+		bullets.y[i] -=1;
+		draw_bullet(i);
+	}
+	for(var i = 0; i < enemy.x.length; i ++){
+		enemy.y[i] += 1;
+		draw_enemy(i);
+	}
 }
 
+let bullets = new Object();
+bullets.x = [];
+bullets.y = [];
 
+let enemy = new Object();
+enemy.x = [];
+enemy.y = [];
 
+function randomNumber(min,max){
+	number = Math.floor(Math.random() * (max - min) + min);
+	return(number);
+}
+function create_enemies(){
+	enemy.x.push(randomNumber(0,405-318/8));
+	enemy.y.push(0);
+}
+function draw_enemy(i){
+	ctx.drawImage(enemyImage,enemy.x[i] ,enemy.y[i], enemyImage.width/8, enemyImage.height/8);	
+}
 
+function check_collision(){
+	for(var i = 0; i < bullets.x.length; i++){
+		for(var  j=0; j < enemy.x.length; j++){
+			
+		}
+	}
+}
 
 
 
 function mainGameLoop(){
-	//Checks time since last loop and if it's been 1/60 of a second, then it draws everything again
-	if(new Date().getTime() - lagtime > 1000/60){
-		ctx.clearRect(0,0,canvas.width,canvas.height);
-		draw_background();
-		draw_characters();
-		parse_buttons();
-		parse_gun();
+	ctx.clearRect(0,0,canvas.width,canvas.height);
+	draw_background();
+	draw_characters();
+	parse_buttons();
+	update();
 
-		lagtime = new Date().getTime();
-	}
-	setTimeout("mainGameLoop();", 1);
 }
-mainGameLoop();
 
+//this parse gun is outside of the main loop because i wanted to lower
+//the fire rate of the gun to prevent lag
+setInterval("create_enemies();", 1000);
+setInterval("parse_gun();", 500);
+setInterval("mainGameLoop();", 1000/60);
 
 
 
